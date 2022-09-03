8cbf5ba3977bd4c34e6269c73aa8caa65a3680eb
Henry Winters <hlwinters17@gmail.com>
2022-09-02 21:52:31 -0700

    Completed home section and start about me component - added programming language display

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHtml5, faCss3Alt, faSquareJs, faReact, faNode, faSquareGit, faGithub} from '@fortawesome/free-brands-svg-icons'
 
+
 const About = () => {
     return (
         <div className='about-section'> 
@@ -12,42 +13,45 @@ const About = () => {
                 <h1>Skills</h1> 
                 <div className='skills'>
                     <div className='skill'>
-                        <FontAwesomeIcon className='contact-icon' icon={faHtml5} />
+                        <i className="devicon-html5-plain colored"></i>
                         <p>HTML5</p> 
                     </div> 
                     <div className='skill'> 
-                        <FontAwesomeIcon className='contact-icon' icon={faCss3Alt} />
+                        <i className="devicon-css3-plain colored"></i>
                         <p>CSS3</p> 
                     </div> 
                     <div className='skill'>
-                        <FontAwesomeIcon className='contact-icon' icon={faSquareJs} />
+                        <i className="devicon-javascript-plain colored"></i>
                         <p>JavaScript</p> 
                     </div> 
                     <div className='skill'>
-                        <FontAwesomeIcon className='contact-icon' icon={faReact} />
+                        <i className="devicon-react-original colored"></i>
                         <p>React</p> 
                     </div> 
                     <div className='skill'>
-                        <FontAwesomeIcon className='contact-icon' icon={faNode} />
+                        <i className="devicon-nodejs-plain colored"></i>
                         <p>Node.js</p>
                     </div> 
                     <div className='skill'> 
+                        <i className="devicon-express-original colored"></i>
                         <p>Express.js</p>
                     </div> 
-                    <div className='skill'> 
+                    <div className='skill'>
+                        <i className="devicon-mongodb-plain colored"></i>
                         <p>MongoDB</p>
                     </div> 
                     <div className='skill'>
+                        <img id='jest-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
                         <p>Jest</p>
                     </div>
                     <div className='skill'>
-                        <FontAwesomeIcon className='contact-icon' icon={faSquareGit} />
+                        <i className="devicon-git-plain colored"></i>
                         <p>Git</p> 
                     </div> 
                     <div className='skill'>
-                        <FontAwesomeIcon className='contact-icon' icon={faGithub} />
+                        <i className="devicon-github-original colored"></i>
                         <p>Github</p>
-                    </div> 
+                    </div>
                 </div> 
             </div> 
         </div> 
